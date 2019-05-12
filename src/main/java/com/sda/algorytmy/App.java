d20a00657dad23edab2adc2fa4c81a17650b3461
Anna Smolinska <anias05@tlen.pl>
2019-05-12 10:02:17 +0100

    fibonacci nowa wiadomosc

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,12 +2,15 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int tab[] = new int[20];
+        tab[0]=0;
+        tab[1]=1;
+        for (int i = 2; i < tab.length; i++) {/// i=2 bo 0 i 1 mamy juz podane
+            tab[i]=tab[i-1]+tab[i-2];
+            System.out.println("to jest: " + tab[i]);
+        }
     }
 }
