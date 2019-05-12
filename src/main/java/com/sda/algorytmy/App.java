1ef56b45e65b098c2b364887bdb9b022c9e51361
Anna Smolinska <anias05@tlen.pl>
2019-05-12 10:26:08 +0100

    fibonacci wersja 3

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -4,12 +4,21 @@ package com.sda.algorytmy;
  * Hello world!
  */
 public class App {
+    public  static  int fibonacci (int n){
+        if (n<=2){
+            return 1;
+        }else {
+            return fibonacci(n-2) + fibonacci(n-1);
+        }
+    }
     public static void main(String[] args) {
+        System.out.println(fibonacci(5));
+
         int tab[] = new int[20];
-        tab[0]=0;
-        tab[1]=1;
+        tab[0] = 0;
+        tab[1] = 1;
         for (int i = 2; i < tab.length; i++) {/// i=2 bo 0 i 1 mamy juz podane
-            tab[i]=tab[i-1]+tab[i-2];
+            tab[i] = tab[i - 1] + tab[i - 2];
             System.out.println("to jest: " + tab[i]);
         }
     }
