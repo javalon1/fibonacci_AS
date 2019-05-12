b7484464660ac3f2e9bf34713ec9e10341ce954c
Anna Smolinska <anias05@tlen.pl>
2019-05-12 11:38:44 +0100

    fibonacci wersja 4

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,17 +1,46 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
 public class App {
-    public  static  int fibonacci (int n){
-        if (n<=2){
+    public static int fibonacci(int n) {
+        if (n <= 2) {
             return 1;
-        }else {
-            return fibonacci(n-2) + fibonacci(n-1);
+        } else {
+            return fibonacci(n - 2) + fibonacci(n - 1);
         }
     }
+
+    public static void fibonacci2() {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        if (N < 2) {
+            //tak
+            System.out.println(N);
+        } else {
+            //Nie
+            int A = 0;
+            int B = 1;
+            int C = 1;
+            int K = 2;
+
+            while (K < N) {
+                A = B;
+                B = C;
+                C = A + B;
+                K++;
+            }
+            //nie, petla sie konczy
+            System.out.println("Wynik: "+C);
+
+        }
+    }
+
     public static void main(String[] args) {
+        fibonacci2();
         System.out.println(fibonacci(5));
 
         int tab[] = new int[20];
